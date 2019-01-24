f63c0ab957b864e5e2dec30c1a064e719d2373fb
= <=>
2019-01-24 14:22:58 +0800

    list finish

--- src/page/detail/Detail.vue
+++ src/page/detail/Detail.vue
@@ -1,22 +1,45 @@
 <template>
   <div>
-   <detail-banner></detail-banner>
-   <detail-header></detail-header>
-   <div class="content"></div>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 <script>
-import DetailBanner from "./components/banner";
-import DetailHeader from "./components/header";
+import DetailBanner from "./components/Banner";
+import DetailHeader from "./components/Header";
+import DetailList from "./components/List";
 export default {
   name: "Detail",
-  components:{
-      DetailBanner,
-      DetailHeader
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [{ title: "成人三馆连票",children:[{ title: "成人三馆连票 -- 某一连锁店"}]}, { title: "成人五馆连票" }]
+        },
+        {
+          title: "学生票"
+        },
+        {
+          title: "儿童票"
+        },
+        {
+          title: "特惠票"
+        }
+      ]
+    };
   }
 };
 </script>
 <style lang="stylus" scoped>
 .content
-  height 50rem
+  height: 50rem
 </style>
