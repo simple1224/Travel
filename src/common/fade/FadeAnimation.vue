c5d909452f76fceed8c5f2f25a31918446633c56
= <=>
2019-01-24 15:25:48 +0800

    animation finish

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 单个组件动画用transition -->
+    <transition>
+        <solt></solt>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name:FadeAnimation
+};
+</script>
+
+<style lang="stylus" scoped>
+.v-enter,.v-leave-to
+  opacity 0
+.v-enter-active,.v-leave-active
+  transition opacity .5s
+</style>
